d2537e8b91b5cecbd30f755ec67cc62d34ce4c6c
Genesis801 <debajyotiroy21@gmail.com>
2021-06-08 10:13:00 +0530

    arithmetic package added

---
+++ SampleJenkinsProject/src/com/capgemini/arithmetic/Additions.java
@@ -0,0 +1,10 @@
+package com.capgemini.arithmetic;
+
+public class Additions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is addition");
+	}
+
+}
